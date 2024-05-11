5404d9503b34ea380930533d606d62059b464851
Wesley Johanson <wesleyJohansonZero@gmail.com>
2024-05-11 13:16:22 -0700

    starting saturday coding, quick save of what I had going on before I started

--- reactor_fxns.m
+++ reactor_fxns.m
@@ -1,11 +1,8 @@
 
-
-
 function fxns = reactor_fxns() 
     fxns.get_supercritical_c02_density = @get_supercritical_c02_density;
 
 end
-
 
 function rho = get_supercritical_c02_density(condition, opt)
     % Input: condition = T or P. Depending on option
@@ -20,11 +17,13 @@ function rho = get_supercritical_c02_density(condition, opt)
     
     if opt == 'isothermal'
         P = condition;
-        rho = 
+        rho.kg_m3 = 1.8853 * P - 31.755;
     else if opt == 'isobaric'
-
+        T = condition;
+        rho.kg_m3 = 0.037 * T^2 - 10.46 * T + 1060.8;
     else
         disp("SUPERCRICIAL C02 DENSITY FUNCTION ERROR: invalid opt")
+        rho = NaN;
     end
 
 end
