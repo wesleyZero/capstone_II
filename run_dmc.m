9c6b602cac94ce72fe58a457234bdfbcea6ee008
Wesley Johanson <wesleyJohansonZero@gmail.com>
2024-05-18 21:25:32 -0700

    removed the level 2 code because we won't need it

--- run_dmc.m
+++ run_dmc.m
@@ -2,7 +2,6 @@ clc; clear; close all;
 
 % SCRIPT________________________________________________________________________
 
-level2();
 level3();
 
 % FUNCTIONS_____________________________________________________________________
@@ -49,6 +48,7 @@ function [F, F_rxtr, R] = level3_flowrates(F, s, chi)
         % Ethylene carbonate is recycled to completion
         % ?? Level 3 tho ??? 
     F = flowrate_fxns().set_F_mol(F);
+    % I SHOULD CALL THIS P
 
     R = get_recycle_flowrates(F, s, chi);
 
@@ -74,40 +74,4 @@ function F = get_recycle_flowrates(F, s, chi)
     R.carbon_dioxide.mol = F.dimethyl_carbonate.mol * (((MR + 1)/s) - 1);
     R.ethylene_carbonate.mol = (F.dimethyl_carbonate.mol / s) * ( (1-chi) / chi);
     R = flowrate_fxns().set_F_mol(R);
-end
-
-function void = level2()
-    console = get_console();
-    const = get_constants(); 
-    user = get_user_inputs();
-    F_fxns = flowrate_fxns();
-    F = user.level2.feed_stream;
-
-    console.section("Starting Level 2 calculations")
-
-    for s = user.level2.selectivity_range
-        F = level2_flowrates(F, s);
-    end
-    
-    console.section("Level 2 calculations are complete")
-end
-
-function F = level2_flowrates(F, s)
-    % Specify Constraints in kta
-    F.dimethyl_carbonate.kta = 100;
-
-    % Update the flowstream  
-    F = flowrate_fxns().set_F_kta(F);
-    
-    % Calculate the molar flowrates as a result of reaction
-    F.ethylene_glycol.mol = F.dimethyl_carbonate.mol;
-    F.carbon_dioxide.mol = F.dimethyl_carbonate.mol;
-    F.ethylene_oxide.mol = F.dimethyl_carbonate.mol / s;
-    F.methoxy_ethanol.mol = F.dimethyl_carbonate.mol * ((1/s) - 1);
-    F.methanol.mol = F.dimethyl_carbonate.mol * ( 1 + (1/s));
-    F.ethylene_carbonate.mol = 0 ;
-        % Ethylene carbonate is recycled to completion
-
-    % Update the flowstream
-    F = flowrate_fxns().set_F_mol(F);
 end
